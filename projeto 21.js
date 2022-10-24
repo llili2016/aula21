9780304db1b256245a9d45ffa992b3cb5663b90e
ll <106106626+llili2016@users.noreply.github.com>
2022-10-24 14:55:09 -0300

    Add files via upload

---
+++ projeto 21.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var  ball, groud0bj, leftSide, rightSide;
+var  world;
+var  radius = 40;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	ground0bj = new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+	rightSide = new ground(1350, 600, 20, 120);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  groundObj.display();
+ 
+}
+
+
+
